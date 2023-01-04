db0d0f8bda7a9847bdd6d0d6e15f10e2bd028772
NguyenBaoTrung080402 <106371674+NguyenBaoTrung080402@users.noreply.github.com>
2023-01-04 19:09:02 +0700

    sử dụng hàm có sẵn

---
+++ module1/git_HTML/bài tập/Bài tập vè mảng/bai1/index.js
@@ -0,0 +1,14 @@
+let color = ["Red", "Blue", "Black", "White","Green"];
+document.getElementById("text").innerText = color.toString();
+// let value;
+// let color = Array();
+
+// value = prompt("Nhập giá số phẩn tử trong mảng: ")
+// for (let i = 0; i < value; i++) {
+//    color=  prompt("Nhập những màu mà bạn biết: ");
+//    document.write(color[i]);
+// }
+// for (let i = 0; i < value; i++) {
+
+//     document.getElementById("text").innerText = color[i].toString();
+// }
